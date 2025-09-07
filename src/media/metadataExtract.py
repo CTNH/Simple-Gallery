2ddec4a6a05455673a5dd0eb049e2c330c4399af
Hopkins <>
2025-09-07 20:18:56 +0800

    Add dependencies function to extract metadata from images

---
+++ src/media/metadataExtract.py
@@ -0,0 +1,78 @@
+from PIL import Image
+from pillow_heif import register_heif_opener
+from os.path import getctime, getmtime, getsize
+from datetime import datetime as dt
+from datetime import timezone as tz
+import exifread
+
+
+# Support for HEIF formats
+register_heif_opener()
+
+IMG_EXT = [
+    ".jpg", ".jpeg",
+    ".png",
+    ".webp",
+    ".heif", ".heic",
+    ".gif",
+    ".bmp"
+]
+VID_EXT = [
+    ".mov",
+    ".mp4",
+    ".webm",
+    ".mkv"
+]
+
+
+def MetaExtract(fpath: str) -> dict:
+    # Extension of file
+    ext = fpath[fpath.rfind("."):].lower()
+
+    # Images
+    if ext in IMG_EXT:
+        img = Image.open(fpath)
+        # Basic metadata
+        data = {
+            "size": img.size,
+            "height": img.height,
+            "width": img.width,
+            "animated": getattr(img, "is_animated", False),
+            "frames": getattr(img, "n_frames", 1)
+        }
+        # EXIF metadata
+        with open(fpath, 'rb') as f:
+            tags = exifread.process_file(f, details=False)
+            for tag in tags:
+                data[tag.split(" ")[1]] = tags[tag]
+
+        fileTime = min(getctime(fpath), getmtime(fpath))
+        # No EXIF for datetime
+        if "DateTime" not in data:
+            data["DateTime"] = fileTime
+        # Timezone exists
+        elif "OffsetTime" in data:
+            data["DateTime"] = dt.strptime(
+                f"{data["DateTime"]} {data["OffsetTime"]}",
+                "%Y:%m:%d %H:%M:%S %z"
+            ).timestamp()
+        # Datetime exists with no timezone
+        else:
+            # Convert to epoch
+            epochTime = dt.strptime(data["DateTime"], "%Y:%m:%d %H:%M:%S")
+            # Set timezone to UTC
+            data["DateTime"] = epochTime.replace(tzinfo=tz.utc).timestamp()
+
+        # Replace resolution size to file size
+        data["size"] = getsize(fpath)
+
+        return data
+
+    # Videos
+    elif ext in VID_EXT:
+        return None
+
+    # Neither
+    else:
+        return None
+
