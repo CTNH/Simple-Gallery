70a16ce102065a313ffc6a9d1992163a70f6b3c0
Hopkins <>
2025-09-09 03:46:29 +0800

    Change size argument in image thumbnail extract from max to min image side size

--- src/media/thumbnails.py
+++ src/media/thumbnails.py
@@ -5,11 +5,15 @@ from src.utils.paths import CreatePath
 def ImgThumbnail(imgPath: str, thumbnailPath: str, size: tuple[int, int]):
     # Copy of original to prevent modification
     img = Image.open(imgPath).copy()
-    img.thumbnail(size)
+    w, h = img.width, img.height
+    if w < h:
+        size = int((h / w) * size[0])
+    else:
+        size = int((w / h) * size[0])
+    img.thumbnail((size, size))
 
     CreatePath(thumbnailPath)
 
     # Disgard alpha channel in case of jpg
     img = img.convert("RGB")
     img.save(thumbnailPath)
-
